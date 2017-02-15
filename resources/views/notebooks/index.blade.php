d8174ec87b64f9988e5e93b2c6289c7d3a85a77a
Kiran <kumar.alg2@gmail.com>
2017-02-15 18:59:41 +0530

    - Using sections and invoking the file in the base.blade.php

--- resources/views/notebooks/index.blade.php
+++ resources/views/notebooks/index.blade.php
@@ -1,30 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-            <title>
-                NoteBook App
-            </title>
-            <link href="{{asset('dist/css/main.css')}}" rel="stylesheet">
-                <link href="{{asset('dist/css/bootstrap.css')}}" rel="stylesheet">
-                </link>
-            </link>
-        </meta>
-    </head>
-    <body>
-        <div class="container-fluid">
-            <nav class="navbar navbar-dark bg-primary">
-                <button aria-controls="navbar-header" class="navbar-toggler hidden-sm-up" data-target="#navbar-header" data-toggle="collapse" type="button">
-                    ☰
-                </button>
-                <div class="collapse navbar-toggleable-xs" id="navbar-header">
-                    <a class="navbar-brand" href="#">
-                        NoteBook App
-                    </a>
-                </div>
-            </nav>
+
+@extends('layouts.base')
             <!-- /navbar -->
             <!-- Main component for call to action -->
+    @section('content')
             <div class="container text-center">
                 <!-- heading -->
                 <h1 class="pull-xs-left">
@@ -71,10 +49,5 @@
                
             </div>
             <!-- /container -->
-            <script src="{{asset('dist/js/jquery3.min.js')}}">
-            </script>
-            <script src="{{asset('dist/js/bootstrap.js')}}">
-            </script>
-        </div>
-    </body>
-</html>
+            
+@endsection
