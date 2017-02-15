20fa281a82208d858b0ef260057de9289c3e8a47
Kiran <kumar.alg2@gmail.com>
2017-02-15 18:48:56 +0530

    - Using blade to move the entire content to a single base.blade.php file - Creating a new folder "layouts" and a new file "base.blade.php"

--- resources/views/frontpage.blade.php
+++ resources/views/frontpage.blade.php
@@ -1,25 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <title>NoteBook App</title>
-    <link rel="stylesheet" href="{{asset('dist/css/main.css')}}">
-    <link rel="stylesheet" href="{{asset('dist/css/bootstrap.css')}}">
-</head>
-
-<body>
-    <div class="container-fluid">
-        <nav class="navbar  navbar-dark bg-primary">
-            <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#navbar-header" aria-controls="navbar-header">
-                &#9776;
-            </button>
-            <div class="collapse navbar-toggleable-xs" id="navbar-header">
-                <a class="navbar-brand" href="#">NoteBook App</a>
-               
-        </nav>
-        <!-- /navbar -->
-        <!-- Main component for call to action -->
+@extends('layouts.base')
+<!--Creating a section that will used in the base.blade.php-->
+@section('content')
         <div class="jumbotron">
             <h1>Notebook</h1>
             <p>Store and organise your thoughts in notebook and NoteBook web app makes this easier than ever</p>
@@ -28,10 +10,5 @@
             </p>
         </div>
     </div>
-    <!-- /container -->
+@endsection
 
-    <script src="{{asset('dist/js/jquery3.min.js')}}"></script>
-    <script src="{{asset('dist/js/bootstrap.js')}}"></script>
-</body>
-
-</html>
